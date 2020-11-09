e596242816804f20e45c36386d1b65129a8ded8c
wax <maxime.abehserra@gmail.com>
2020-11-09 19:58:49 +0100

    some refactorign on routes : separation of logic insides controllers

--- routes/auth.js
+++ routes/auth.js
@@ -1,85 +1,34 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
 
 const { isAuthenticated } = require('../middlewares/permissions');
-const ENV = require('../ENV');
-const Recruiter = require('../models/recruiter');
 
-// @route     GET api/auth
-// @desc      Get logged in user
-// @access    Private
-router.get('/', isAuthenticated, async (req, res) => {
-    try {
-        const user = await Recruiter.findById(req.user._id).select('-password');
-        res.json(user);
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Server Error');
-    }
-});
+const authControllers = require("../controllers/auth");
 
-router.post('/register', async (req, res) => {
-    try {
-        const { name, lastname, username, password } = req.body
-        const user = await Recruiter.findOne({ username: username })
+router
+    // @route     GET api/auth
+    // @desc      Get logged in user
+    // @access    Private
+    .get('/', isAuthenticated, authControllers.getLoggedInUser)
 
-        if (user) return res.status(401).send({ status: 'FAILED', msg: 'username already exist' });
+    // @route     POST api/auth/register
+    // @desc      Register to Api with credentials
+    // @access    Public
+    .post('/register', authControllers.register)
 
-        const hashedPassword = await bcrypt.hash(password, 10);
+    // @route     POST api/auth/login
+    // @desc      Login to Api with credentials
+    // @access    Public
+    .post('/login', authControllers.login)
 
-        const newUser = new Recruiter({
-            name, lastname, username,
-            password: hashedPassword
-        });
-        const saved_user = await newUser.save();
-        res.status(200).send({ status: 'SUCCESS', data: saved_user });
+    // @route     GET api/auth/logout
+    // @desc      Logout to Api with credentials
+    // @access    Private
+    .get('/logout', authControllers.logout)
 
-    } catch (error) {
-        console.log(`err : ${error}`);
-        res.status(500).send({ status: 'FAILED', msg: error })
-    }
-})
-
-    .post('/login', async (req, res) => {
-        console.log('LOGIN ROUTE')
-        try {
-            const { username, password } = req.body;
-            console.log(username, password)
-            const user = await Recruiter.findOne({ username: username });
-            if (!user) return res.status(401).send({ status: 'failed', msg: 'user does not exist' });
-
-            const match = await bcrypt.compare(password, user.password);
-            if (!match) return res.status(401).send({ status: 'failed', msg: 'wrong password or username ' });
-
-            const token = await jwt.sign({ username: user.username, _id: user._id }, ENV.SECRET_KEY, { expiresIn: '1h' });
-            res.status(200).send({ status: 'success', data: user, token })
-        } catch (error) {
-            res.status(500).send({ status: 'failed', msg: error })
-        }
-    })
-
-    .get('/logout', async (req, res) => {
-        res.json({ action: 'logout' })
-    })
-
-    .delete('/deleteProfile', isAuthenticated, async (req, res) => {
-        const { _id } = req.user,
-            { password } = req.body;
-        try {
-            const user = await Recruiter.findOne({ _id });
-            const isPasswordValid = await bcrypt.compare(password, user.password)
-            if (isPasswordValid) {
-                const removedUser = await Recruiter.deleteOne({ _id });
-                res.json(removedUser);
-            } else {
-                throw new Error('you need to enter your password to delete your profile')
-            }
-        } catch (error) {
-            console.log(error)
-            res.status(401).send({ status: 'FAILED', msg: error.message, type: 'DELETE_PROFILE' })
-        }
-    })
+    // @route     DELETE api/auth/deleteProfile
+    // @desc      Delete user profile 
+    // @access    Private
+    .delete('/deleteProfile', isAuthenticated, authControllers.deleteUser)
 
 module.exports = router; 
--- routes/projects.js
+++ routes/projects.js
@@ -1,91 +1,43 @@
 const express = require('express');
 const router = express.Router();
-const Job = require('../models/job');
-const Application = require('../models/application');
 
-/**
- * GET ALL PROJECT FROM A USER
- * GET api/projects
- */
-router.get('/', async (req, res) => {
-    const { _id } = req.user;
-    const options = req.query;
-    try {
-        const result = await Job.find({ recruiterId: _id }).populate({ path: 'applications', populate: { path: 'candidate_id' } }).exec();
-        res.status(200).send(result)
-    } catch (error) {
-        console.log(error)
-    }
-})
+const controller = require('../controllers/projects');
 
-/**
- * POST A NEW PROJECT
- * POST api/project/new
- */
-router.post('/new', async (req, res) => {
-    const { title, required_tech, required_tools, localisation, salary } = req.body;
-    const { _id } = req.user;
-    try {
-        const project = new Job({
-            title,
-            required_skills: { tech: required_tech, tools: required_tools },
-            localisation: { city: localisation },
-            salary,
-            recruiterId: _id
-        })
-        const saved_project = await project.save();
-        console.log(saved_project);
-        res.status(200).send(saved_project);
-    } catch (error) {
-        console.log(error);
-        res.status(401).send(error)
-    }
-})
+router
+    /**
+     * @route GET api/projects
+     * @desc GET ALL PROJECT FROM A USER
+     * @access Private
+     */
+    .get('/', controller.getAllProject)
 
-router.put('/update:id', async (req, res) => {
+    /**
+     * @route POST api/projects/new
+     * @desc POST A NEW PROJECT
+     * @access Private
+     */
+    .post('/new', controller.createNewProject)
 
-})
+    /**
+     * @route PUT api/projects/update:id
+     * @desc Update a project 
+     * @access Private
+     */
+    .put('/update:id', controller.updateProject)
 
-/**
- * ADD APPLICATION TO A PROJECT [:id]
- * POST api/projects/addApplication:id 
- */
-router.post('/:project_id/addApplication/:candidate_id', async (req, res) => {
-    const { project_id, candidate_id } = req.params;
-    try {
-        // Check if candidate application already exist for this project 
-        const current_project = await Job.findOne({ _id: project_id }).populate({ path: 'applications' }).exec();
-        const doesApplicationAlreadyExist = current_project.applications.filter(app => app.candidate_id.toString() === candidate_id)
-        if (doesApplicationAlreadyExist.length >= 1) return res.send({ status: 'error', msg: 'ce candidat est déjà rattaché à ce projet ' })
+    /**
+     * @route POST api/projects/:project_id/addApplication:candidate_id 
+     * @desc  ADD APPLICATION TO A PROJECT [:id]
+     * @access Private
+     */
+    .post('/:project_id/addApplication/:candidate_id', controller.CreateNewApplication)
 
-        // Save New application
-        const new_application = new Application({
-            candidate_id,
-            status: 'created'
-        })
-        const application = await new_application.save();
-        
-        // add new application reference to project 
-        current_project.applications.push(application._id);
-        const updated_project = await current_project.save();
-
-        res.send({ updated_project });
-    } catch (error) {
-        console.log(error)
-    }
-})
-
-/**
- * DELETE PROJECT 
- * DELETE api/projects/delete:id
- */
-router.delete('/delete:id', async (req, res) => {
-    const { id } = req.params;
-    console.log(id);
-    const deleted_doc = await Job.findByIdAndDelete(id);
-    console.log(deleted_doc);
-    res.status(200).send('deleted ' + id + 'from DB')
-})
+    /**
+     * @route DELETE api/projects/delete:id
+     * @desc DELETE PROJECT 
+     * @access Private
+     */
+    .delete('/delete:id', controller.deleteProject)
 
 
 module.exports = router;
